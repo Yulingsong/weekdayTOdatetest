9ea5709ba050d76a7deda7d3dfce4ef62ca3f39d
Yulingsong <yulingsong5@126.com>
2016-01-11 12:53:58 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by yulingsong on 16/1/11.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by yulingsong on 16/1/11.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
